218baa55c5a3d79584f5b5715bc15d19ac55493b
Frank Pizzella <fpizz210@gmail.com>
2023-02-26 22:25:27 -0500

    Player Data DB

--- server/database.ts
+++ server/database.ts
@@ -1,25 +1,41 @@
-const sqlite3 = require('sqlite3').verbose()
-const DBSOURCE = "db.sqlite"
+import { Database } from "sqlite3";
 
-let db = new sqlite3.Database(DBSOURCE, (err: Error) => {
-    if (err) {
-        // Cannot open database
-        console.error(err.message)
-        throw err
-    } else {
-        console.log('Connected to the SQLite database.')
-        db.run(`CREATE TABLE IF NOT EXISTS players (
+const sqlite3 = require("sqlite3").verbose();
+const DBSOURCE = "db.sqlite";
+
+const db = new Database(DBSOURCE, (err: Error | null) => {
+  if (err) {
+    // Cannot open database
+    console.error(err.message);
+    throw err;
+  } else {
+    console.log("Connected to the SQLite database.");
+    db.run(
+      `CREATE TABLE IF NOT EXISTS players (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             name text NOT NULL
             )`,
-            (err: Error) => {
-                if (err) {
-                    // Table already created
-                } else {
-                    // Table just created, creating some rows
-                }
-            });
-    }
+      (err: Error) => {
+        if (err) {
+          // Table already created
+        } else {
+          // Table just created, creating some rows
+        }
+      }
+    );
+    db.run(
+      `CREATE TABLE IF NOT EXISTS playerData (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            playerID INTEGER,
+            timeStamp INTEGER NOT NULL,
+            X INTEGER NOT NULL,
+            Y INTEGER NOT NULL,
+            Z INTEGER NOT NULL,
+            V INTEGER NOT NULL,
+            FOREIGN KEY(playerID) REFERENCES players(id)
+    )`
+    );
+  }
 });
 
-module.exports = db
+module.exports = db;
--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,5 @@
-import express, { Express, Request, Response } from 'express';
-import dotenv from 'dotenv';
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
 
 dotenv.config();
 
@@ -7,98 +7,169 @@ const app: Express = express();
 const port = 8000;
 
 const db = require("./database.js");
-const cors = require('cors');
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
+
+app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Express + TypeScript Server');
+app.get("/", (req: Request, res: Response) => {
+  res.send("Express + TypeScript Server");
 });
 
 app.listen(port, () => {
-    console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
+
+let playerID = 1;
+
+const updateID = (id: number) => {
+  playerID = id;
+};
 
 // Fix Types
 
 // Insert here other API endpoints
 app.get("/api/players", (req, res, next) => {
-    const sql = "select * from players"
-    db.all(sql, (err: Error, rows: any[]) => {
-        if (err) {
-            res.status(400).json({ "error": err.message });
-            return;
-        }
-        res.json({
-            "message": "success",
-            "data": rows
-        })
+  const sql = "select * from players";
+  db.all(sql, (err: Error, rows: any[]) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: "success",
+      data: rows,
     });
+  });
+});
+
+app.post("/updateID", (req, res, next) => {
+  playerID = req.body.id;
+  updateID(playerID);
+  res.json({
+    message: "success",
+    data: playerID,
+  });
 });
 
 app.get("/api/players/:id", (req, res, next) => {
-    const sql = "select * from players where id = ?"
-    const params = [req.params.id]
-    db.get(sql, params, (err: Error, row: any[]) => {
-        if (err) {
-            res.status(400).json({ "error": err.message });
-            return;
-        }
-        res.json({
-            "message": "success",
-            "data": row
-        })
+  const sql = "select * from players where id = ?";
+  const params = [req.params.id];
+  db.get(sql, params, (err: Error, row: any[]) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: "success",
+      data: row,
     });
+  });
 });
 
-// app.post("/api/players/", (req, res, next) => {
-//     var errors=[]
-//     if (!req.body.name){
-//         errors.push("No name specified");
-//     }
-//     if (errors.length){
-//         res.status(400).json({"error":errors.join(",")});
-//         return;
-//     }
-//     var data = {
-//         name: req.body.name
-//     }
-//     var sql ='INSERT INTO players (name) VALUES (?)'
-//     var params =[data.name]
-//     db.run(sql, params, function (err, result) {
-//         if (err){
-//             res.status(400).json({"error": err.message})
-//             return;
-//         }
-//         res.json({
-//             "message": "success",
-//             "data": data,
-//             "id" : this.lastID
-//         })
-//     });
-// })
+app.post("/api/players/", (req, res, next) => {
+  const errors: String[] = [];
+  if (!req.body.name) {
+    errors.push("No name specified");
+  }
+  if (errors.length) {
+    res.status(400).json({ error: errors.join(",") });
+    return;
+  }
+  const data = {
+    name: req.body.name,
+  };
+  const sql = "INSERT INTO players (name) VALUES (?)";
+  const params = [data.name];
+  db.run(sql, params, function (err: Error, result: any) {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: "success",
+      data: data,
+      id: db.lastID,
+    });
+  });
+});
 
-// app.patch("/api/players/:id", (req, res, next) => {
-//     var data = {
-//         name: req.body.name
-//     }
-//     db.run(
-//         `UPDATE players set
-//            name = COALESCE(?,name),
-//            WHERE id = ?`,
-//         [data.name, req.params.id],
-//         function (err, result) {
-//             if (err){
-//                 res.status(400).json({"error": res.message})
-//                 return;
-//             }
-//             res.json({
-//                 message: "success",
-//                 data: data,
-//                 changes: this.changes
-//             })
-//     });
-// })
-// // Default response for any other request
-// app.use(function(req, res){
-//     res.status(404);
-// });
+type PlayerData = {
+  playerID: number;
+  timeStamp: number;
+  X: number;
+  Y: number;
+  Z: number;
+  V: number;
+};
+
+app.post("/api/playerData/", (req, res, next) => {
+  const errors: String[] = [];
+  if (!req.body.ts) {
+    errors.push("No name specified");
+  }
+  if (!req.body.xyzv) {
+    errors.push("Malformed Body");
+  }
+  if (errors.length) {
+    res.status(400).json({ error: errors.join(",") });
+    return;
+  }
+  if (req.body.bball === "NULL") {
+    return;
+  }
+  const realData = req.body.xyzv.find(
+    (data: number[]) => data[3] === req.body.bball
+  );
+  const data: PlayerData = {
+    playerID: playerID,
+    timeStamp: req.body.ts,
+    X: realData[0],
+    Y: realData[1],
+    Z: realData[2],
+    V: realData[3],
+  };
+  const sql =
+    "INSERT INTO playerData (playerID, timeStamp, X, Y, Z, V) VALUES (?,?,?,?,?,?)";
+  const params = [
+    data.playerID,
+    data.timeStamp,
+    data.X,
+    data.Y,
+    data.Z,
+    data.V,
+  ];
+  db.run(sql, params, function (err: Error, result: any) {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: "success",
+      data: data,
+      id: db.lastID,
+    });
+  });
+});
+
+app.get("/api/playerData/:id", (req, res, next) => {
+  const sql = "select * from playerData where id = ?";
+  const params = [req.params.id];
+  db.get(sql, params, (err: Error, row: any[]) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: "success",
+      data: row,
+    });
+  });
+});
