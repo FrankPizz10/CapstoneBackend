bc4efb9c7c5e453e864dfaf123ac0d3e7d7ff808
Frank Pizzella <fpizz210@gmail.com>
2023-03-02 12:24:01 -0500

    Extra Data

--- server/SeedDatabase.ts
+++ server/SeedDatabase.ts
@@ -1,38 +1,70 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 export async function seedData() {
-    await addPlayerData();
+  await addPlayerData();
+  await addExtraData();
 }
 
 async function addPlayerData() {
-    const playerData = {
-        "Frank": [new Date(1).toISOString(), 1, 1, 1, 1],
-        "Arthur": [new Date(2).toISOString(), 2, 2, 2, 2],
-        "Nick": [new Date(3).toISOString(), 3, 3, 3, 3],
-        "John": [new Date(4).toISOString(), 4, 4, 4, 4],
-        "Joe": [new Date(5).toISOString(), 5, 5, 5, 5],
+  const playerData = {
+    Frank: [new Date(1).toISOString(), 1, 1, 1, 1],
+    Arthur: [new Date(2).toISOString(), 2, 2, 2, 2],
+    Nick: [new Date(3).toISOString(), 3, 3, 3, 3],
+    John: [new Date(4).toISOString(), 4, 4, 4, 4],
+    Joe: [new Date(5).toISOString(), 5, 5, 5, 5],
+  };
+  for (const name in playerData) {
+    try {
+      const player = await prisma.player.create({
+        data: {
+          name,
+        },
+      });
+      await prisma.playerdata.create({
+        data: {
+          playerId: player.id,
+          timeStamp: playerData[
+            name as keyof typeof playerData
+          ][0] as unknown as Date,
+          X: playerData[
+            name as keyof typeof playerData
+          ][1] as unknown as number,
+          Y: playerData[
+            name as keyof typeof playerData
+          ][2] as unknown as number,
+          Z: playerData[
+            name as keyof typeof playerData
+          ][3] as unknown as number,
+          V: playerData[
+            name as keyof typeof playerData
+          ][4] as unknown as number,
+        },
+      });
+    } catch (e) {
+      continue;
     }
-    for (const name in playerData) {
-        try {
-            const player = await prisma.player.create({
-                data: {
-                    name,
-                },
-            });
-            await prisma.playerdata.create({
-                data: {
-                    playerId: player.id,
-                    timeStamp: playerData[name as keyof typeof playerData][0] as unknown as Date,
-                    X: playerData[name as keyof typeof playerData][1] as unknown as number,
-                    Y: playerData[name as keyof typeof playerData][2] as unknown as number,
-                    Z: playerData[name as keyof typeof playerData][3] as unknown as number,
-                    V: playerData[name as keyof typeof playerData][4] as unknown as number,
-                }
-            });
-        } catch (e) {
-            continue;
-        }
-    }
+  }
+}
+
+async function addExtraData() {
+  const player = await prisma.player.findUnique({
+    where: {
+      name: "Frank",
+    },
+  });
+  if (player) {
+    console.log("Here");
+    const playerData = await prisma.playerdata.create({
+      data: {
+        playerId: player.id,
+        timeStamp: new Date(6).toISOString(),
+        X: 10,
+        Y: 10,
+        Z: 10,
+        V: 100,
+      },
+    });
+  }
 }
--- server/index.ts
+++ server/index.ts
@@ -14,131 +14,130 @@ const bodyParser = require("body-parser");
 const prisma = new PrismaClient();
 
 seedData().then(() => {
-    console.log("Seeded");
+  console.log("Seeded");
 });
 
 app.use(
-    bodyParser.urlencoded({
-        extended: false,
-    })
+  bodyParser.urlencoded({
+    extended: false,
+  })
 );
 
 app.use(bodyParser.json());
 app.use(cors());
 
 app.get("/", (req: Request, res: Response) => {
-    res.send("Express + TypeScript Server");
+  res.send("Express + TypeScript Server");
 });
 
 app.listen(port, () => {
-    console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
 
 let playerID = 1;
 
 const updateID = (id: number) => {
-    playerID = id;
+  playerID = id;
 };
 
 app.get("/api/players", async (req, res, next) => {
-    const players = await prisma.player.findMany();
-    res.json({
-        message: "success",
-        data: players,
-    });
+  const players = await prisma.player.findMany();
+  res.json({
+    message: "success",
+    data: players,
+  });
 });
 
 app.get("/api/players/:id", async (req, res, next) => {
-    const player = await prisma.player.findUnique({
-        where: {
-            id: Number(req.params.id),
-        },
-    });
-    console.log(player);
-    res.json({
-        message: "success",
-        data: player,
-    });
+  const player = await prisma.player.findUnique({
+    where: {
+      id: Number(req.params.id),
+    },
+  });
+  console.log(player);
+  res.json({
+    message: "success",
+    data: player,
+  });
 });
 
+// Add Player
 app.post("/api/players/", async (req, res, next) => {
-    const errors: String[] = [];
+  const errors: String[] = [];
 
-    if (!req.body.name) {
-        errors.push("No name specified");
+  if (!req.body.name) {
+    errors.push("No name specified");
+  }
+  if (errors.length) {
+    res.status(400).json({ error: errors.join(",") });
+    return;
+  }
+  try {
+    const player = await prisma.player.create({
+      data: {
+        name: req.body.name,
+      },
+    });
+    res.json({
+      message: "success",
+      data: player,
+    });
+  } catch (e) {
+    if (e instanceof Prisma.PrismaClientKnownRequestError) {
+      if (e.code === "P2002") {
+        res.status(400).json({ error: "Name already exists" });
+      }
     }
-    if (errors.length) {
-        res.status(400).json({ error: errors.join(",") });
-        return;
-    }
-    try {
-        const player = await prisma.player.create({
-            data: {
-                name: req.body.name,
-            },
-        });
-        res.json({
-            message: "success",
-            data: player,
-        });
-    }
-    catch (e) {
-        if (e instanceof Prisma.PrismaClientKnownRequestError) {
-            if (e.code === 'P2002') {
-                res.status(400).json({ error: "Name already exists" });
-            }
-        }
-    }
+  }
 });
 
 app.post("/api/playerData/", async (req, res, next) => {
-    const errors: String[] = [];
-    if (!req.body.ts) {
-        errors.push("No name specified");
-    }
-    if (!req.body.xyzv) {
-        errors.push("Malformed Body");
-    }
-    if (errors.length) {
-        res.status(400).json({ error: errors.join(",") });
-        return;
-    }
-    if (req.body.bball === "NULL") {
-        res.status(400).json({ error: "No bball" });
-        return;
-    }
-    const spacialData = req.body.xyzv.find(
-        (data: number[]) => data[3] === req.body.bball
-    );
-    if (spacialData) {
-        const playerData = await prisma.playerdata.create({
-            data: {
-                playerId: playerID,
-                timeStamp: new Date(req.body.ts),
-                X: spacialData[0],
-                Y: spacialData[1],
-                Z: spacialData[2],
-                V: spacialData[3],
-            },
-        });
-        res.json({
-            message: "success",
-            data: playerData,
-        });
-    }
-    else {
-        res.status(400).json({ error: "No data for bball" });
-    }
+  const errors: String[] = [];
+  if (!req.body.ts) {
+    errors.push("No name specified");
+  }
+  if (!req.body.xyzv) {
+    errors.push("Malformed Body");
+  }
+  if (errors.length) {
+    res.status(400).json({ error: errors.join(",") });
+    return;
+  }
+  if (req.body.bball === "NULL") {
+    res.status(400).json({ error: "No bball" });
+    return;
+  }
+  const spacialData = req.body.xyzv.find(
+    (data: number[]) => data[3] === req.body.bball
+  );
+  if (spacialData) {
+    const playerData = await prisma.playerdata.create({
+      data: {
+        playerId: playerID,
+        timeStamp: new Date(req.body.ts),
+        X: spacialData[0],
+        Y: spacialData[1],
+        Z: spacialData[2],
+        V: spacialData[3],
+      },
+    });
+    res.json({
+      message: "success",
+      data: playerData,
+    });
+  } else {
+    res.status(400).json({ error: "No data for bball" });
+  }
 });
 
 app.get("/api/playerData/:id", async (req, res, next) => {
-    const playerData = await prisma.playerdata.findMany({
-        where: {
-            playerId: Number(req.params.id),
-        },
-    });
-    res.json({
-        message: "success",
-        data: playerData,
-    });
+  const playerData = await prisma.playerdata.findMany({
+    where: {
+      playerId: Number(req.params.id),
+    },
+  });
+  res.json({
+    message: "success",
+    data: playerData,
+  });
 });
