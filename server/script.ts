40c7b7822ac8d07199c67e29ae8e343c66272906
Frank Pizzella <fpizz210@gmail.com>
2023-03-01 20:08:56 -0500

    Seed Data Not Working

--- server/script.ts
+++ server/script.ts
@@ -3,8 +3,6 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 async function main() {
-    const deleteAll = await prisma.player.deleteMany({});
-    const deleteAllData = await prisma.playerdata.deleteMany({});
     await addDefaultPlayers();
     await addDefaultPlayerData();
 }
@@ -21,59 +19,72 @@ main()
 
 async function addDefaultPlayers() {
     const defaultPlayers = ['Frank', 'Arthur', 'Nick', 'John', 'Joe']
+    let id = 1;
     for (const player of defaultPlayers) {
-        const createPlayer = await prisma.player.create({
-            data: {
-                name: player,
-            },
-        })
-        console.log(createPlayer)
+        let playerExists;
+        try {
+            playerExists = await prisma.player.findFirst({
+                where: {
+                    id: id,
+                    name: player,
+                },
+            });
+        }
+        catch (e) {
+            continue;
+        };
+        if (!playerExists) {
+            const createPlayer = await prisma.player.create({
+                data: {
+                    name: player,
+                },
+            });
+            console.log(createPlayer);
+        }
+        else {
+            console.log('Player already exists');
+        }
+        id++;
     }
 }
 
 async function addDefaultPlayerData() {
-    const ids = await prisma.player.findMany({
-        select: {
-            id: true,
-        },
-    })
-    if (ids.length < 5) {
-        throw new Error('Not enough players in database')
-    }
     const defaultPlayerData = [
-        [ids[0].id, 1, 1, 1, 1, 1],
-        [ids[1].id, 2, 2, 2, 2, 2],
-        [ids[2].id, 3, 3, 3, 3, 3],
-        [ids[3].id, 4, 4, 4, 4, 4],
-        [ids[4].id, 5, 5, 5, 5, 5],
+        [1, 1, 1, 1, 1, 1],
+        [2, 2, 2, 2, 2, 2],
+        [3, 3, 3, 3, 3, 3],
+        [4, 4, 4, 4, 4, 4],
+        [5, 5, 5, 5, 5, 5],
+        [1, 10, 10, 10, 10, 10],
     ]
     for (const data of defaultPlayerData) {
-        const createPlayerData = await prisma.playerdata.create({
-            data: {
-                playerId: data[0],
-                timeStamp: data[1],
-                X: data[2],
-                Y: data[3],
-                Z: data[4],
-                V: data[5],
-            },
-        })
-        console.log(createPlayerData)
+        let playerDataExists;
+        try {
+            playerDataExists = await prisma.playerdata.findFirst({
+                where: {
+                    playerId: data[0],
+                    timeStamp: data[1],
+                },
+            });
+        }
+        catch (e) {
+            continue;
+        };
+        if (!playerDataExists) {
+            const createPlayerData = await prisma.playerdata.create({
+                data: {
+                    playerId: data[0],
+                    timeStamp: data[1],
+                    X: data[2],
+                    Y: data[3],
+                    Z: data[4],
+                    V: data[5],
+                },
+            });
+            console.log(createPlayerData)
+        }
+        else {
+            console.log('Player data already exists');
+        }
     }
-    const playerID = await prisma.player.findFirst({
-        select: {
-            id: true,
-        },
-    })
-    const createPlayerData = await prisma.playerdata.create({
-        data: {
-            playerId: playerID!.id,
-            timeStamp: 10,
-            X: 10,
-            Y: 10,
-            Z: 10,
-            V: 10,
-        },
-    })
-    console.log(createPlayerData)
 }
